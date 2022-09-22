a7c7758e145feeccf5553c890a9f36bfdf35bb81
Mike <reinstein.mike@gmail.com>
2022-09-22 10:23:44 -0700

    update plugin to work with postcss 8

--- index.m.js
+++ index.m.js
@@ -1,27 +1,31 @@
-'use strict'
-
-import postcss from 'postcss'
+import postcss  from 'postcss'
 import CleanCss from 'clean-css'
 
-const initializer = (opts = {}) => {
+
+const plugin = (opts = {}) => {
   const cleancss = new CleanCss(opts)
 
-  return (css, res) => {
-    return new Promise((resolve, reject) => {
-      cleancss.minify(css.toString(), (err, min) => {
-        if (err) {
-          return reject(new Error(err.join('\n')))
-        }
+  return {
+    postcssPlugin: 'postcss-clean',
+    Once (css, { result }) {
+      return new Promise((resolve, reject) => {
+        cleancss.minify(css.toString(), (err, min) => {
+          if (err) {
+            return reject(new Error(err.join('\n')))
+          }
 
-        for (let w of min.warnings) {
-          res.warn(w)
-        }
+          for (let w of min.warnings) {
+            result.warn(w)
+          }
 
-        res.root = postcss.parse(min.styles)
-        resolve()
+          result.root = postcss.parse(min.styles)
+          resolve()
+        })
       })
-    })
+    }
   }
 }
 
-export default postcss.plugin('clean', initializer)
+plugin.postcss = true
+
+export default plugin
